27bafc340ef470184fa3b37956f63e1bccef4f2d
software8686 <software8686@gmail.com>
2019-12-24 03:21:27 +0330

    Commit> Room Dependencies & Set Layers

---
+++ app/src/main/java/ir/sample/roomsample/roomLayer/dataAccessObjects/UserDao.java
@@ -0,0 +1,29 @@
+package ir.sample.roomsample.roomLayer.dataAccessObjects;
+
+import java.util.List;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import ir.sample.roomsample.roomLayer.entities.UserEntity;
+
+@Dao
+public interface UserDao {
+
+    @Query("select * from users")
+    public List<UserEntity> getAll();
+
+    @Insert
+    public void insert(UserEntity user);
+
+    @Update
+    public void update(UserEntity user);
+
+    @Delete
+    public void delete(UserEntity user);
+
+    @Query("delete from users")
+    public void deleteAll();
+
+}
---
+++ app/src/main/java/ir/sample/roomsample/roomLayer/entities/UserEntity.java
@@ -0,0 +1,26 @@
+package ir.sample.roomsample.roomLayer.entities;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "users")
+public class UserEntity {
+    @PrimaryKey(autoGenerate = true) public int id;
+    @ColumnInfo(name = "user_name") public String name;
+    @ColumnInfo(name = "user_email") public String email;
+    @ColumnInfo(name = "user_number") public String number;
+
+    public UserEntity(String name, String email, String number) {
+        this.name = name;
+        this.email = email;
+        this.number = number;
+    }
+
+    public UserEntity(int id, String name, String email, String number) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.number = number;
+    }
+}
